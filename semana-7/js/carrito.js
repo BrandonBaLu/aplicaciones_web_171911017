75ea23023bce7a94cadba49826994a2913f56c83
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-10-29 20:29:16 +0000

    p

--- semana-7/js/carrito.js
+++ semana-7/js/carrito.js
@@ -3,28 +3,26 @@ function insertar(){
   var producto= document.getElementById("producto");
   var selected = producto.options[producto.selectedIndex].text;
   var cantidad= $("#cantidad_product").val();
-  var total= cantidad * precio
-  var texto = "Tus compras son:"+"\nLa cantidad de productos es : "+ cantidad + "\nEl producto es: " + selected +  "\nEl precio es de:$" + precio + "\nEl total es de:$"+ total+"\n";
-  var otro = $("#comment").val()+ texto;
-  var totalcompra=total
-  if(cantidad<10 & cantidad >0 &  precio >0) 
+  var total= cantidad * precio ;
+  var carrito= $("#comment").val();
   {
-  
+    $("#comment").text(carrito + cantidad )
     document.getElementById("comment").innerHTML = otro;
   }
   else if (cantidad <=0 & precio<=0 ){
-    alert("No has ingresado valores")
+    Swal.fire("Error","No has ingresado valores", 'error')
   }
 
   else if (cantidad>0 & cantidad>=10 & precio>0){
-    alert("La cantidad de productos es igual o mayor a 10")
+    Swal.fire("Error", "La cantidad del producto debe ser mayor a 0 y menor a 10", 'error');
   }
 
   else if (precio>0 & cantidad<=0 || precio<=0 & cantidad>0 ){
-    alert("No has insertado valores completos")
+    Swal.fire("Error","No has insertado valores completos", 'error')
   }
 }
 
 function limpiar(){
   $("#comment").empty();
 };
+
